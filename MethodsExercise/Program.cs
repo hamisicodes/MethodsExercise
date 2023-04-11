84e7e6e9af714595497ce7b8726022affdeb7388
Hamisi Ngao <newhamisi@gmail.com>
2023-04-10 22:13:40 -0400

    uncomment soln

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -32,19 +32,19 @@
         }
         static void Main(string[] args)
         {
-            // Console.WriteLine("Enter your name");
-            // string name = Console.ReadLine();
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
 
-            // Console.WriteLine("Enter your favorite color");
-            // string favoriteColor = Console.ReadLine();
+            Console.WriteLine("Enter your favorite color");
+            string favoriteColor = Console.ReadLine();
 
-            // Console.WriteLine("Enter your favorite animal");
-            // string favoriteAnimal = Console.ReadLine();
+            Console.WriteLine("Enter your favorite animal");
+            string favoriteAnimal = Console.ReadLine();
 
-            // Console.WriteLine("Whats your favorite band");
-            // string favoriteBand = Console.ReadLine();
+            Console.WriteLine("Whats your favorite band");
+            string favoriteBand = Console.ReadLine();
 
-            // Console.WriteLine($"My name is {name}. My favourite color is {favoriteColor}. Of all animals , {favoriteAnimal} is my favorite. I also enjoy music. {favoriteBand} is my favorite band.");
+            Console.WriteLine($"My name is {name}. My favourite color is {favoriteColor}. Of all animals , {favoriteAnimal} is my favorite. I also enjoy music. {favoriteBand} is my favorite band.");
 
             Console.WriteLine($"Addition: {Add(2,3)}");
             Console.WriteLine($"Addition: {Subtract(20,3)}");
