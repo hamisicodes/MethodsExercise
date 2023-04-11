e6b4fd8271a0dcb275a6a564833e29c6f74f1742
Hamisi Ngao <newhamisi@gmail.com>
2023-04-10 21:34:28 -0400

    Record user input, for each variable. Combine all answers for a fun, short story

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter your favorite color");
+            string favoriteColor = Console.ReadLine();
+
+            Console.WriteLine("Enter your favorite animal");
+            string favoriteAnimal = Console.ReadLine();
+
+            Console.WriteLine("Whats your favorite band");
+            string favoriteBand = Console.ReadLine();
+
+            Console.WriteLine($"My name is {name}. My favourite color is {favoriteColor}. Of all animals , {favoriteAnimal} is my favorite. I also enjoy music. {favoriteBand} is my favorite band".);
+
         }
     }
 }
